4093fd23e48710d71a117e1a003657195f13b69b
bap-ba-bip-boop <42996283+bap-ba-bip-boop@users.noreply.github.com>
2021-10-05 11:29:16 +0200

    Added 3 methods

--- Grupp2Inlämn/BankLogic.cs
+++ Grupp2Inlämn/BankLogic.cs
@@ -2,22 +2,22 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Grupp2Inlämn
-{
-    class BankLogic
-    {
-        private List<Customer> Customers;
-
-        public List<string> GetCustomers() { }
-        public bool AddCustomer() { }
-        public List<string> GetCustomer() { }
-        public bool ChangeCustomerName() { }
-        public List<string> RemoveCustomer() { }
-        public int AddSavinsAccount() { }
-        public string GetAccount() { }
-        public bool Deposit() { }
-        public bool Withdraw() { }
-        public string CloseAccount() { }
-
-    }
-}
+//namespace Grupp2Inlämn
+//{
+//    class BankLogic
+//    {
+//        private List<Customer> Customers;
+//
+//        public List<string> GetCustomers() { }
+//        public bool AddCustomer() { }
+//        public List<string> GetCustomer() { }
+//        public bool ChangeCustomerName() { }
+//        public List<string> RemoveCustomer() { }
+//        public int AddSavinsAccount() { }
+//        public string GetAccount() { }
+//        public bool Deposit() { }
+//        public bool Withdraw() { }
+//        public  CloseAccount() { }
+//
+//    }
+//}
--- Grupp2Inlämn/SavingsAccount.cs
+++ Grupp2Inlämn/SavingsAccount.cs
@@ -6,16 +6,36 @@ namespace Grupp2Inlämn
 {
     class SavingsAccount
     {
-        private double Saldo;
+        private double Amount;
         private double Interest;
         private string AccountType;
-        private int Kontonummer;
+        private int AccountNr;
 
-        public void deposit() { }
-        public void withdraw() { }
-        public void getAccountNum() { }
-        public void getInterest() { }
-        public void getInfo() { }
+        public void deposit() { }//Pierre o niklas
+        public void withdraw() { }//Pierre o niklas
+        public int getAccountNum()
+        {
+            //returnar AccountNr till användaren(i detta fall BankLogic)
+            return this.AccountNr;
+        }
+        public double getInterest() 
+        {
+            //returnar räntan till användaren(till BankLogic). ränta i kr
+            return this.Amount*this.Interest/100.0;
+        }
+        public string getInfo()
+        {
+            //returnerar parametrarna i följande ordning
+            //(kontonummer, saldo, kontotyp, räntesats).
+            return this.AccountNr + ", " + this.Amount + ", " + this.AccountType + ", " + this.Interest;
+        }
 
+        public SavingsAccount()
+        {
+            this.AccountNr = 1001;
+            this.Amount = 1000.0;
+            this.AccountType = "Sparkonto";
+            this.Interest = 1.0;
+        }
     }
 }
