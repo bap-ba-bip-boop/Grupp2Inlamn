8ce84f769d642bc3d59ffde3993ad18cdd774662
bap-ba-bip-boop <42996283+bap-ba-bip-boop@users.noreply.github.com>
2021-10-05 10:28:41 +0200

    Add project files.

---
+++ Grupp2Inlämn/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Grupp2Inlämn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
