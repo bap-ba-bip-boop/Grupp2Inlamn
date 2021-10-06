94ee7f2f12d808cd56b9fa8a940164b84a4c96a0
bap-ba-bip-boop <42996283+bap-ba-bip-boop@users.noreply.github.com>
2021-10-06 09:17:50 +0200

    Added BankLogic Method Descriptions

--- Grupp2Inlämn/BankLogic.cs
+++ Grupp2Inlämn/BankLogic.cs
@@ -2,22 +2,61 @@
 using System.Collections.Generic;
 using System.Text;
 
-//namespace Grupp2Inlämn
-//{
-//    class BankLogic
-//    {
-//        private List<Customer> Customers;
-//
-//        public List<string> GetCustomers() { }
-//        public bool AddCustomer() { }
-//        public List<string> GetCustomer() { }
-//        public bool ChangeCustomerName() { }
-//        public List<string> RemoveCustomer() { }
-//        public int AddSavinsAccount() { }
-//        public string GetAccount() { }
-//        public bool Deposit() { }
-//        public bool Withdraw() { }
-//        public  CloseAccount() { }
-//
-//    }
-//}
+namespace Grupp2Inlämn
+{
+    class BankLogic
+    {
+        private List<Customer> Customers;
+
+        /*
+         Tar bort kund med personnummer pNr ur banken, alla kundens eventuella konton tas också bort
+och resultatet returneras. Listan som returneras ska innehålla information om alla konton som togs
+bort, saldot som kunden får tillbaka samt vad räntan blev.
+        */
+        public List<string> GetCustomers() { }
+
+        /*Tar bort kund med personnummer pNr ur banken, alla kundens eventuella konton tas också bort
+och resultatet returneras. Listan som returneras ska innehålla information om alla konton som togs
+bort, saldot som kunden får tillbaka samt vad räntan blev.*/
+        public bool AddCustomer() { }
+
+        /*Tar bort kund med personnummer pNr ur banken, alla kundens eventuella konton tas också bort
+och resultatet returneras. Listan som returneras ska innehålla information om alla konton som togs
+bort, saldot som kunden får tillbaka samt vad räntan blev.*/
+        public List<string> GetCustomer() { }
+
+        /*Tar bort kund med personnummer pNr ur banken, alla kundens eventuella konton tas också bort
+och resultatet returneras. Listan som returneras ska innehålla information om alla konton som togs
+bort, saldot som kunden får tillbaka samt vad räntan blev.*/
+        public bool ChangeCustomerName() { }
+
+        /*Tar bort kund med personnummer pNr ur banken, alla kundens eventuella konton tas också bort
+och resultatet returneras. Listan som returneras ska innehålla information om alla konton som togs
+bort, saldot som kunden får tillbaka samt vad räntan blev.*/
+        public List<string> RemoveCustomer() { }
+
+        /*Tar bort kund med personnummer pNr ur banken, alla kundens eventuella konton tas också bort
+och resultatet returneras. Listan som returneras ska innehålla information om alla konton som togs
+bort, saldot som kunden får tillbaka samt vad räntan blev.*/
+        public int AddSavinsAccount() { }
+
+        /*Tar bort kund med personnummer pNr ur banken, alla kundens eventuella konton tas också bort
+och resultatet returneras. Listan som returneras ska innehålla information om alla konton som togs
+bort, saldot som kunden får tillbaka samt vad räntan blev.*/
+        public string GetAccount() { }
+
+        /*Tar bort kund med personnummer pNr ur banken, alla kundens eventuella konton tas också bort
+och resultatet returneras. Listan som returneras ska innehålla information om alla konton som togs
+bort, saldot som kunden får tillbaka samt vad räntan blev.*/
+        public bool Deposit() { }
+        /*Tar bort kund med personnummer pNr ur banken, alla kundens eventuella konton tas också bort
+och resultatet returneras. Listan som returneras ska innehålla information om alla konton som togs
+bort, saldot som kunden får tillbaka samt vad räntan blev.*/
+        public bool Withdraw() { }
+        /*Tar bort kund med personnummer pNr ur banken, alla kundens eventuella konton tas också bort
+och resultatet returneras. Listan som returneras ska innehålla information om alla konton som togs
+bort, saldot som kunden får tillbaka samt vad räntan blev.*/
+        public CloseAccount() { }
+
+    }
+}
